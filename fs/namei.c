c9660d7b5ac5fb126d8ef1e08186c8132f161391
flar2 <asegaert@gmail.com>
2013-07-31 23:48:37 -0400

    Linaro 4.8 fixes
    Conflicts:
    	drivers/net/phy/phy.c
    
    Change-Id: I1711655670bfabfe6b2faffcd143d174fe73602b

--- fs/namei.c
+++ fs/namei.c
@@ -2745,7 +2745,7 @@ out:
 static long do_rmdir(int dfd, const char __user *pathname)
 {
 	int error = 0;
-	char * name;
+	char * name = NULL;
 	struct dentry *dentry;
 	struct nameidata nd;
 
@@ -2841,7 +2841,7 @@ int vfs_unlink(struct inode *dir, struct dentry *dentry)
 static long do_unlinkat(int dfd, const char __user *pathname)
 {
 	int error;
-	char *name;
+	char *name = NULL;
 	struct dentry *dentry;
 	struct nameidata nd;
 	struct inode *inode = NULL;
@@ -3241,8 +3241,8 @@ SYSCALL_DEFINE4(renameat, int, olddfd, const char __user *, oldname,
 	struct dentry *old_dentry, *new_dentry;
 	struct dentry *trap;
 	struct nameidata oldnd, newnd;
-	char *from;
-	char *to;
+	char *from = NULL;
+	char *to = NULL;
 	int error;
 
 	error = user_path_parent(olddfd, oldname, &oldnd, &from);
