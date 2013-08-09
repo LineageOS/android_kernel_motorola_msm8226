281958185fcd1e53b2264dbca9a9a6ed52262374
Arif Hussain <c_arifh@qca.qualcomm.com>
2013-08-09 15:09:58 -0700

    wlan: Setting Max Tx Power Limit for specific band
    Added two commands to set Max Tx Power for a specific band
    
    Change-Id: Ie79a3f28f99f04848b69acee4811b69e13e14ac3
    CRs-Fixed: 517289

--- CORE/WDI/CP/inc/wlan_qct_wdi_i.h
+++ CORE/WDI/CP/inc/wlan_qct_wdi_i.h
@@ -455,6 +455,9 @@ typedef enum
   /* WLAN FW set batch scan request */
   WDI_SET_BATCH_SCAN_REQ                        = 86,
 
+  /*WLAN DAL Set Max Tx Power Per band Request*/
+  WDI_SET_MAX_TX_POWER_PER_BAND_REQ             = 87,
+
   WDI_MAX_REQ,
 
   /*Send a suspend Indication down to HAL*/
@@ -750,6 +753,7 @@ typedef enum
 
   WDI_SET_BATCH_SCAN_RESP                       = 85,
 
+  WDI_SET_MAX_TX_POWER_PER_BAND_RSP             = 86,
   /*-------------------------------------------------------------------------
     Indications
      !! Keep these last in the enum if possible
@@ -2243,6 +2247,22 @@ WDI_ProcessNvDownloadReq
  @return Result of the function call
 */
 WDI_Status WDI_ProcessSetMaxTxPowerReq
+(
+  WDI_ControlBlockType*  pWDICtx,
+  WDI_EventInfoType*     pEventData
+);
+
+/**
+ @brief Process Set Max Tx Power Per Band Request function (called when Main
+        FSM allows it)
+
+ @param  pWDICtx:         pointer to the WLAN DAL context
+         pEventData:      pointer to the event information structure
+
+ @see
+ @return Result of the function call
+*/
+WDI_Status WDI_ProcessSetMaxTxPowerPerBandReq
 (
   WDI_ControlBlockType*  pWDICtx,
   WDI_EventInfoType*     pEventData
