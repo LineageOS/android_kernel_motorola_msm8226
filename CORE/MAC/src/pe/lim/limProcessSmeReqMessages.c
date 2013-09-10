8e360d3d3960d575cd784e298e0523485502c930
Madan Mohan Koyyalamudi <mkoyyala@qca.qualcomm.com>
2013-09-10 21:51:12 +0530

    wlan: Revert back changes setting the gLimRspReqd for stats requests
    The link speed is showing as 0 in the display as the function
    limProcessNormalHddMsg(pMac, limMsg, false) is being
    called with fRspReqd as false for eWNI_SME_STA_STAT_REQ.
    Changes are made to revert back freeing the request when fRspReqd
    is false in functions __limProcessSmeGetStatisticsRequest,
    __limProcessSmeGetRoamRssiRequest and __limProcessSmeStatsRequest.
    
    Change-Id: Ieee05233f3535a77a6237478b0d25e697f5d3006
    CRs-Fixed: 541869

--- CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
+++ CORE/MAC/src/pe/lim/limProcessSmeReqMessages.c
@@ -4059,16 +4059,6 @@ __limProcessSmeStatsRequest(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
             return;
     }
 
-    if ( !pMac->lim.gLimRspReqd )
-    {
-        vos_mem_free( pMsgBuf );
-        return;
-    }
-    else
-    {
-        pMac->lim.gLimRspReqd = FALSE;
-    }
-
     msgQ.reserved = 0;
     msgQ.bodyptr = pMsgBuf;
     msgQ.bodyval = 0;
@@ -4114,16 +4104,6 @@ __limProcessSmeGetStatisticsRequest(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
 
     msgQ.type = WDA_GET_STATISTICS_REQ;    
 
-    if ( !pMac->lim.gLimRspReqd )
-    {
-        vos_mem_free( pMsgBuf );
-        return;
-    }
-    else
-    {
-        pMac->lim.gLimRspReqd = FALSE;
-    }
-
     msgQ.reserved = 0;
     msgQ.bodyptr = pMsgBuf;
     msgQ.bodyval = 0;
@@ -4160,16 +4140,6 @@ __limProcessSmeGetRoamRssiRequest(tpAniSirGlobal pMac, tANI_U32 *pMsgBuf)
 
     pPEGetRoamRssiReq = (tpAniGetRssiReq) pMsgBuf;
     msgQ.type = WDA_GET_ROAM_RSSI_REQ;
-
-    if ( !pMac->lim.gLimRspReqd )
-    {
-        vos_mem_free( pMsgBuf );
-        return;
-    }
-    else
-    {
-        pMac->lim.gLimRspReqd = FALSE;
-    }
 
     msgQ.reserved = 0;
     msgQ.bodyptr = pMsgBuf;
