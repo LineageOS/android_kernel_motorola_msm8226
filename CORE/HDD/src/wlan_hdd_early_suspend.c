a939aecf98f3cd52f52eee4058376eec75eb0c49
Agarwal Ashish <ashishka@codeaurora.org>
2013-10-30 20:11:12 +0530

    wlan: HDD log level modification.
    Added logs which should get printed when some unexpected error
    happens. Modification in log level for HDD.
    
    CRs-Fixed: 517829
    Change-Id: I5da6044a28b2f225ccdb51da184c8665895d4b67

--- CORE/HDD/src/wlan_hdd_early_suspend.c
+++ CORE/HDD/src/wlan_hdd_early_suspend.c
@@ -976,7 +976,7 @@ VOS_STATUS hdd_conf_arp_offload(hdd_adapter_t *pAdapter, int fenable)
        }
        else
        {
-           hddLog(VOS_TRACE_LEVEL_INFO, FL("IP Address is not assigned"));
+           hddLog(VOS_TRACE_LEVEL_ERROR, FL("IP Address is not assigned"));
        }
 
        if (fenable == 1 && !pAdapter->ipv4_notifier_registered)
@@ -1557,7 +1557,8 @@ void hdd_resume_wlan(void)
       return;
    }
    
-   if (pHddCtx->isLogpInProgress) {
+   if (pHddCtx->isLogpInProgress)
+   {
       hddLog(VOS_TRACE_LEVEL_INFO,
              "%s: Ignore resume wlan, LOGP in progress!", __func__);
       return;
