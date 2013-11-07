2197d6ed1c3fb5f66037a0c45c31ea288578ac27
Kiet Lam <c_kietl@qca.qualcomm.com>
2013-11-07 12:56:49 +0530

    WLAN: Fix memory leaks during get BSS Descriptor IE's
    1.Free 'ppIEStruct' allocated in csrGetParsedBssDescriptionIEs,
    in all instances.
    2.Free previous memory (wdiPrefNetworkFoundInd.pData) on a failed
    allocation for pPrefNetworkFoundInd in WDA_lowLevelIndCallback
    3.Replace the returns with a break in __limProcessSmeAssocCnfNew
    to ensure the memory to get freed.
    
    Change-Id: I97c4035ffb62235be83daca9c65b4068011b5cbd
    CRs-Fixed: 568201

--- CORE/SME/src/csr/csrApiScan.c
+++ CORE/SME/src/csr/csrApiScan.c
@@ -3940,7 +3940,7 @@ tANI_BOOLEAN csrLearnCountryInformation( tpAniSirGlobal pMac, tSirBssDescription
                     (tSirMacChanInfo *)(&pIesLocal->Country.triplets[0]) ))
         {
             fRet = eANI_BOOLEAN_FALSE;
-            return fRet;
+            break;
         }
 
         // set the indicator of the channel where the country IE was found...
@@ -7725,6 +7725,11 @@ eHalStatus csrScanSavePreferredNetworkFound(tpAniSirGlobal pMac,
    }
    //Add to scan cache
    csrScanAddResult(pMac, pScanResult, pIesLocal);
+
+   if( (pScanResult->Result.pvIes == NULL) && pIesLocal )
+   {
+       vos_mem_free(pIesLocal);
+   }
 
    vos_mem_free(pParsedFrame);
 
