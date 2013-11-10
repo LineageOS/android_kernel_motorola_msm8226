5a330208ca1579084e8a02f8fe42ff4cdbc44fda
Huajun Li <huajun.li@intel.com>
2013-11-10 23:13:17 +0800

    f2fs: add a new mount option: inline_data
    Add a mount option: inline_data. If the mount option is set,
    data of New created small files can be stored in their inode.
    
    Signed-off-by: Huajun Li <huajun.li@intel.com>
    Signed-off-by: Haicheng Li <haicheng.li@linux.intel.com>
    Signed-off-by: Weihong Xu <weihong.xu@intel.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk.kim@samsung.com>
    
    Conflicts:
    	fs/f2fs/super.c
    
    Change-Id: I17d59035afdc9e04cfda75c446f8d95b3dd1313c

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -54,6 +54,7 @@ enum {
 	Opt_err_continue,
 	Opt_err_panic,
 	Opt_err_recover,
+	Opt_inline_data,
 	Opt_err,
 };
 
@@ -73,6 +74,7 @@ static match_table_t f2fs_tokens = {
 	{Opt_err_continue, "errors=continue"},
 	{Opt_err_panic, "errors=panic"},
 	{Opt_err_recover, "errors=recover"},
+	{Opt_inline_data, "inline_data"},
 	{Opt_err, NULL},
 };
 
@@ -382,7 +384,9 @@ static int parse_options(struct super_block *sb, char *options)
 			} else
 				return -EINVAL;
 			break;
-
+		case Opt_inline_data:
+			set_opt(sbi, INLINE_DATA);
+			break;
 		default:
 			f2fs_msg(sb, KERN_ERR,
 				"Unrecognized mount option \"%s\" or missing value",
@@ -580,6 +584,8 @@ static int f2fs_show_options(struct seq_file *seq, struct dentry *root)
 					F2FS_ANDROID_EMU_NOCASE) ?
 						":nocase" : "");
 
+	if (test_opt(sbi, INLINE_DATA))
+		seq_puts(seq, ",inline_data");
 	seq_printf(seq, ",active_logs=%u", sbi->active_logs);
 
 	return 0;
