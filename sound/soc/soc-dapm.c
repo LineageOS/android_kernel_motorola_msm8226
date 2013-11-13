f45ca3a30cd479d583022dfe8bb1a30b368dccfd
Gopikrishnaiah Anandan <agopik@codeaurora.org>
2013-11-13 11:06:13 -0800

    ASoC: dapm: Change mutex lock subclass in dapm
    Address the compilation issue by changing the subclass
    enum in mixer/mux power update dapm functions
    
    Change-Id: I3946cf2a5c6a0b469378d5c0583fd9c8567d4b70
    CRs-Fixed: 567030
    Signed-off-by: Gopikrishnaiah Anandan <agopik@codeaurora.org>

--- sound/soc/soc-dapm.c
+++ sound/soc/soc-dapm.c
@@ -1968,7 +1968,7 @@ int snd_soc_dapm_mux_update_power(struct snd_soc_dapm_widget *widget,
 {
 	struct snd_soc_card *card = widget->dapm->card;
 	int ret;
-	mutex_lock_nested(&card->dapm_mutex, SND_SOC_DAPM_CLASS_RUNTIME);
+	mutex_lock_nested(&card->dapm_mutex, SND_SOC_DAPM_CLASS_PCM);
 	ret = soc_dapm_mux_update_power(widget, kcontrol, change, mux, e);
 	mutex_unlock(&card->dapm_mutex);
 	return ret;
@@ -2016,7 +2016,7 @@ int snd_soc_dapm_mixer_update_power(struct snd_soc_dapm_widget *widget,
 {
 	struct snd_soc_card *card = widget->dapm->card;
 	int ret;
-	mutex_lock_nested(&card->dapm_mutex, SND_SOC_DAPM_CLASS_RUNTIME);
+	mutex_lock_nested(&card->dapm_mutex, SND_SOC_DAPM_CLASS_PCM);
 	ret = soc_dapm_mixer_update_power(widget, kcontrol, connect);
 	mutex_unlock(&card->dapm_mutex);
 	return ret;
