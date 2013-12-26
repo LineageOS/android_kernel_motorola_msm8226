06cb71d5a1ac9c344c3760245fbd64c701b22a96
Rajeev Kumar <rajekuma@codeaurora.org>
2013-12-26 14:42:24 -0800

    wlan: Fix of crash issue with batch scan disabled
    Removing feature flag from WDI enumeration definitions because even if
    feature is not defined a NULL entry is added in WDI request and response
    function pointer tables and because of it wrong handler is being picked
    up by WDI while handling request messages from UMAC.
    
    Change-Id: I02e4033a1aef71b6806e36b47e071a6055ef05df
    CRs-fixed: 593760

--- CORE/WDI/CP/inc/wlan_qct_wdi_i.h
+++ CORE/WDI/CP/inc/wlan_qct_wdi_i.h
@@ -452,10 +452,8 @@ typedef enum
   /* WLAN FW LPHB config request */
   WDI_LPHB_CFG_REQ                              = 85,
 
-#ifdef FEATURE_WLAN_BATCH_SCAN
   /* WLAN FW set batch scan request */
   WDI_SET_BATCH_SCAN_REQ                        = 86,
-#endif
 
   WDI_MAX_REQ,
 
@@ -480,15 +478,14 @@ typedef enum
   /* Send a delete periodic Tx pattern indicationto HAL */
   WDI_DEL_PERIODIC_TX_PATTERN_IND,
 
-  /*Keep adding the indications to the max request
-    such that we keep them sepparate */
-
-#ifdef FEATURE_WLAN_BATCH_SCAN
   /*Send stop batch scan indication to FW*/
   WDI_STOP_BATCH_SCAN_IND,
+
   /*Send stop batch scan indication to FW*/
   WDI_TRIGGER_BATCH_SCAN_RESULT_IND,
-#endif
+
+  /*Keep adding the indications to the max request
+    such that we keep them sepparate */
 
   WDI_MAX_UMAC_IND
 }WDI_RequestEnumType; 
@@ -748,9 +745,7 @@ typedef enum
   /* WLAN FW LPHB Config response */
   WDI_LPHB_CFG_RESP                             = 84,
 
-#ifdef FEATURE_WLAN_BATCH_SCAN
-  WDI_SET_BATCH_SCAN_RESP                        = 85,
-#endif
+  WDI_SET_BATCH_SCAN_RESP                       = 85,
 
   /*-------------------------------------------------------------------------
     Indications
@@ -812,10 +807,7 @@ typedef enum
   WDI_HAL_PERIODIC_TX_PTRN_FW_IND     = WDI_HAL_IND_MIN + 16,
 
 
-#ifdef FEATURE_WLAN_BATCH_SCAN
   WDI_BATCHSCAN_RESULT_IND           =  WDI_HAL_IND_MIN + 17,
-#endif
-
 
   WDI_MAX_RESP
 }WDI_ResponseEnumType; 
