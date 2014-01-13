043b25a22866da4ad52cc9489fa7bbda14edd9ee
dhacker29 <dhackerdvm@gmail.com>
2014-01-13 04:02:28 -0600

    Revert "msm: vidc: Add control to preserve text quality"
    This reverts commit 6183f35d5b183fe92404e32a53546f8a1de53892.

--- drivers/media/platform/msm/vidc/vidc_hfi_api.h
+++ drivers/media/platform/msm/vidc/vidc_hfi_api.h
@@ -178,7 +178,6 @@ enum hal_property {
 	HAL_PARAM_VDEC_FRAME_ASSEMBLY,
 	HAL_PARAM_VDEC_CONCEAL_COLOR,
 	HAL_PARAM_VENC_H264_VUI_BITSTREAM_RESTRC,
-	HAL_PARAM_VENC_PRESERVE_TEXT_QUALITY,
 };
 
 enum hal_domain {
@@ -813,10 +812,6 @@ struct hal_h264_vui_timing_info {
 };
 
 struct hal_h264_vui_bitstream_restrc {
-	u32 enable;
-};
-
-struct hal_preserve_text_quality {
 	u32 enable;
 };
 
