bc28ad8435aeabbfa67ebf971a7a2017a034bb5e
Krishna Chaitanya Parimi <cparimi@codeaurora.org>
2014-02-05 15:50:31 +0530

    msm: mdss: Correct ARGC read write for all DSPP
    Currently ARGC read write is done for DSPP0 irrespective of
    dspp_num, as the dspp_num corresponding to the block provided
    is not calculated.
    
    Added the calculation of the dspp_num to get correct address
    of ARGC registers.
    
    Change-Id: Id787e76debb26d23d75d3cdb859a697b6f0016f5
    Signed-off-by: Krishna Chaitanya Parimi <cparimi@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_pp.c
+++ drivers/video/msm/mdss/mdss_mdp_pp.c
@@ -2086,6 +2086,12 @@ int mdss_mdp_argc_config(struct mdp_pgc_lut_data *config,
 	mutex_lock(&mdss_pp_mutex);
 
 	disp_num = PP_BLOCK(config->block) - MDP_LOGICAL_BLOCK_DISP_0;
+	ret = pp_get_dspp_num(disp_num, &dspp_num);
+	if (ret) {
+		pr_err("%s, no dspp connects to disp %d", __func__, disp_num);
+		goto argc_config_exit;
+	}
+
 	switch (PP_LOCAT(config->block)) {
 	case MDSS_PP_LM_CFG:
 		argc_offset = MDSS_MDP_REG_LM_OFFSET(dspp_num) +
@@ -2109,12 +2115,6 @@ int mdss_mdp_argc_config(struct mdp_pgc_lut_data *config,
 	tbl_size = GC_LUT_SEGMENTS * sizeof(struct mdp_ar_gc_lut_data);
 
 	if (config->flags & MDP_PP_OPS_READ) {
-		ret = pp_get_dspp_num(disp_num, &dspp_num);
-		if (ret) {
-			pr_err("%s, no dspp connects to disp %d",
-				__func__, disp_num);
-			goto argc_config_exit;
-		}
 		mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 		local_cfg = *config;
 		local_cfg.r_data =
