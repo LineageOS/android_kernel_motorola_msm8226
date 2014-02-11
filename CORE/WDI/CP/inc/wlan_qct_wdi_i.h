e7eba5a3f86b648e0c0dd72d5a1306ffe2e47d4c
Manjunathappa Prakash <c_pmanju@qca.qualcomm.com>
2014-02-10 18:09:15 -0800

    wlan: add update channel list request support
    Add capability to support dynamic updation of scan channel list based
    on the change in country code.
    
    Change-Id: I5d898617932fe07026f1e6423c676a6838ee5405
    CRs-Fixed: 605330
    (cherry picked from commit cfaa366bbf0c3cce6cb55449aef8fd64fb463b54)

--- CORE/WDI/CP/inc/wlan_qct_wdi_i.h
+++ CORE/WDI/CP/inc/wlan_qct_wdi_i.h
@@ -458,6 +458,8 @@ typedef enum
   /*WLAN DAL Set Max Tx Power Per band Request*/
   WDI_SET_MAX_TX_POWER_PER_BAND_REQ             = 87,
 
+  WDI_UPDATE_CHAN_REQ                           = 88,
+
   WDI_MAX_REQ,
 
   /*Send a suspend Indication down to HAL*/
@@ -754,6 +756,9 @@ typedef enum
   WDI_SET_BATCH_SCAN_RESP                       = 85,
 
   WDI_SET_MAX_TX_POWER_PER_BAND_RSP             = 86,
+
+  WDI_UPDATE_CHAN_RESP                          = 87,
+
   /*-------------------------------------------------------------------------
     Indications
      !! Keep these last in the enum if possible
@@ -2813,6 +2818,22 @@ WDI_Status WDI_ProcessLPHBConfReq
 );
 #endif /* FEATURE_WLAN_LPHB */
 
+/**
+ @brief WDI_ProcessUpdateChannelParamsReq -
+    Send update channel request to FW
+
+ @param  pWDICtx : wdi context
+         pEventData : indication data
+
+ @see
+ @return success or failure
+*/
+WDI_Status WDI_ProcessUpdateChannelParamsReq
+(
+  WDI_ControlBlockType*  pWDICtx,
+  WDI_EventInfoType*     pEventData
+);
+
 #ifdef FEATURE_WLAN_BATCH_SCAN
 /**
  @brief WDI_ProcessSetBatchScanReq -
@@ -3504,6 +3525,23 @@ WDI_ProcessConfigStaRsp
 WDI_Status
 WDI_ProcessSetLinkStateRsp
 ( 
+  WDI_ControlBlockType*  pWDICtx,
+  WDI_EventInfoType*     pEventData
+);
+
+/**
+ @brief Process Update Channel Rsp function (called when a response is
+        being received over the bus from HAL)
+
+ @param  pWDICtx:         pointer to the WLAN DAL context
+         pEventData:      pointer to the event information structure
+
+ @see
+ @return Result of the function call
+*/
+WDI_Status
+WDI_ProcessUpdateChanRsp
+(
   WDI_ControlBlockType*  pWDICtx,
   WDI_EventInfoType*     pEventData
 );
