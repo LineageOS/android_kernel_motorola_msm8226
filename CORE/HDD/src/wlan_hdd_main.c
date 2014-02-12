1a5c024d7a49efc2726df312232c1ee3dd98dba3
Binesh B <binesh@motorola.com>
2014-02-12 15:24:57 +0530

    Merge interface specific filter configuration.
    (CR) : WLAN: Add interface specific filter configuration
    
    Fire v6 filter configuration set onl if the adpater mode is
    INFRA on assoc and multicast listener triggers
    
    Change-Id: I5fa7d7210315a7a5e82040d98d2a01ecd5c2c424
    Reviewed-on: http://gerrit.pcs.mot.com/609972
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Prashanth Chakravarthy <a19110@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-on: http://gerrit.mot.com/652211
    Reviewed-by: Binesh Balasingh <binesh@motorola.com>

--- CORE/HDD/src/wlan_hdd_main.c
+++ CORE/HDD/src/wlan_hdd_main.c
@@ -7826,7 +7826,8 @@ static void hdd_set_multicast_list(struct net_device *dev)
       hddLog(VOS_TRACE_LEVEL_INFO,
             "%s: allow all multicast frames", __func__);
       pAdapter->mc_addr_list.mc_cnt = 0;
-      wlan_hdd_update_v6_filters(pAdapter, 1); // IKJB42MAIN-1244, Motorola, a19091
+      if(pAdapter->device_mode == WLAN_HDD_INFRA_STATION)
+          wlan_hdd_update_v6_filters(pAdapter, 1); // IKJB42MAIN-1244, Motorola, a19091
    }
    else 
    {
@@ -7853,7 +7854,8 @@ static void hdd_set_multicast_list(struct net_device *dev)
                MAC_ADDR_ARRAY(pAdapter->mc_addr_list.addr[i]));
          i++;
       }
-      wlan_hdd_update_v6_filters(pAdapter, 0); // IKJB42MAIN-1244, Motorola, a19091
+      if(pAdapter->device_mode == WLAN_HDD_INFRA_STATION)
+          wlan_hdd_update_v6_filters(pAdapter, 0); // IKJB42MAIN-1244, Motorola, a19091
 
    }
    return;
