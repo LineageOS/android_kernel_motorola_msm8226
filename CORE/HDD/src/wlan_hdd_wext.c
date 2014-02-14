291d9f5a72e0fa7b413a9e984eaef048a864a764
Jeff Johnson <jjohnson@qca.qualcomm.com>
2014-02-14 13:22:22 -0800

    wlan: prima: Fix ambiguous ioctl error logging
    There are multiple places where the message "Invalid IOCTL command" is
    logged.  All of the messages are identical, so when the message is
    actually logged it is impossible to determine exactly where, and hence
    why, it was logged.  Update the logs to add the function name so that
    the source of the log will be unique.
    
    Change-Id: I5906627fc3ebd7896dd35e0b82579d02fa915bc3
    CRs-fixed: 616348

--- CORE/HDD/src/wlan_hdd_wext.c
+++ CORE/HDD/src/wlan_hdd_wext.c
@@ -4502,7 +4502,7 @@ int iw_set_three_ints_getnone(struct net_device *dev, struct iw_request_info *in
 
         default:
         {
-            hddLog(LOGE, "Invalid IOCTL command %d",  sub_cmd );
+            hddLog(LOGE, "%s: Invalid IOCTL command %d", __func__, sub_cmd );
             break;
         }
     }
@@ -4918,7 +4918,7 @@ static int iw_get_char_setnone(struct net_device *dev, struct iw_request_info *i
 #endif
         default:
         {
-            hddLog(LOGE, "Invalid IOCTL command %d",  sub_cmd );
+            hddLog(LOGE, "%s: Invalid IOCTL command %d", __func__, sub_cmd );
             break;
         }
     }
@@ -5269,7 +5269,8 @@ int iw_set_var_ints_getnone(struct net_device *dev, struct iw_request_info *info
 #endif
         default:
             {
-                hddLog(LOGE, "Invalid IOCTL command %d",  sub_cmd );
+                hddLog(LOGE, "%s: Invalid IOCTL command %d",
+                       __func__, sub_cmd );
             }
             break;
     }
