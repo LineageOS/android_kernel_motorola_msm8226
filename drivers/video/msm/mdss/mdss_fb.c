15523b6fc92ab2273bba108542e642bb3fd9fb7f
Jeykumar Sankaran <jsanka@codeaurora.org>
2014-03-06 12:12:24 -0800

    msm: mdss: dsi: Share panel alignment restrictions through sysfs node
    Read panel alignment restrictions on left, top, width and height values of
    the updating ROI from the dtsi file and share through sysfs node.
    Framework needs these details for configuring MDP for certain
    features (e.g. Partial frame update).
    
    Change-Id: Ibe6973b721bb20120a6d7a217bad2d0d159cc836
    Signed-off-by: Jeykumar Sankaran <jsanka@codeaurora.org>
    (cherry picked from commit 0e68156525272d5b47bddbb70c29bdcc2714d54d)
    
    Conflicts:
    	Documentation/devicetree/bindings/fb/mdss-dsi-panel.txt
    	drivers/video/msm/mdss/mdss_dsi_panel.c
    	drivers/video/msm/mdss/mdss_panel.h
    Change-Id: I0e94ec6507640b031fbdc2a5295f6df56239275e
    Reviewed-on: http://gerrit.mot.com/776186
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Fred Fettinger <fettinge@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Stephen Rossbach <rossbach@motorola.com>
    Tested-by: Stephen Rossbach <rossbach@motorola.com>
    Reviewed-by: Zachary Crosby <zac@motorola.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -440,12 +440,32 @@ static ssize_t mdss_fb_get_idle_notify(struct device *dev,
 	return ret;
 }
 
+static ssize_t mdss_fb_get_panel_info(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	struct fb_info *fbi = dev_get_drvdata(dev);
+	struct msm_fb_data_type *mfd = fbi->par;
+	struct mdss_panel_info *pinfo = mfd->panel_info;
+	int ret;
+
+	ret = scnprintf(buf, PAGE_SIZE,
+			"pu_en=%d\nxstart=%d\nwalign=%d\nystart=%d\nhalign=%d\n"
+			"min_w=%d\nmin_h=%d",
+			pinfo->partial_update_enabled, pinfo->xstart_pix_align,
+			pinfo->width_pix_align, pinfo->ystart_pix_align,
+			pinfo->height_pix_align, pinfo->min_width,
+			pinfo->min_height);
+
+	return ret;
+}
+
 static DEVICE_ATTR(msm_fb_type, S_IRUGO, mdss_fb_get_type, NULL);
 static DEVICE_ATTR(msm_fb_split, S_IRUGO, mdss_fb_get_split, NULL);
 static DEVICE_ATTR(show_blank_event, S_IRUGO, mdss_mdp_show_blank_event, NULL);
 static DEVICE_ATTR(idle_time, S_IRUGO | S_IWUSR | S_IWGRP,
 	mdss_fb_get_idle_time, mdss_fb_set_idle_time);
 static DEVICE_ATTR(idle_notify, S_IRUGO, mdss_fb_get_idle_notify, NULL);
+static DEVICE_ATTR(msm_fb_panel_info, S_IRUGO, mdss_fb_get_panel_info, NULL);
 static DEVICE_ATTR(rgb, S_IRUGO | S_IWUSR | S_IWGRP, mdss_get_rgb, mdss_set_rgb);
 
 static struct attribute *mdss_fb_attrs[] = {
@@ -454,6 +474,7 @@ static struct attribute *mdss_fb_attrs[] = {
 	&dev_attr_show_blank_event.attr,
 	&dev_attr_idle_time.attr,
 	&dev_attr_idle_notify.attr,
+	&dev_attr_msm_fb_panel_info.attr,
 	&dev_attr_rgb.attr,
 	NULL,
 };
