199de37aaf1ceb67664f08d1cda3a0f764fa5858
savoca <adeddo27@gmail.com>
2014-03-26 16:49:52 -0400

    LCD_KCAL: Color control driver for Moto G
    Change-Id: Ie529f3ee28926810affed0825510e4395e4bb764

--- arch/arm/mach-msm/board-8226.c
+++ arch/arm/mach-msm/board-8226.c
@@ -58,6 +58,13 @@
 #include "pm.h"
 #include "modem_notifier.h"
 #include "spm-regulator.h"
+#ifdef CONFIG_LCD_KCAL
+#include <mach/kcal.h>
+#include <linux/module.h>
+#include "../../../drivers/video/msm/mdss/mdss_fb.h"
+extern int update_preset_lcdc_lut(void);
+#endif
+
 
 static struct memtype_reserve msm8226_reserve_table[] __initdata = {
 	[MEMTYPE_SMI] = {
@@ -111,6 +118,52 @@ static void __init msm8226_reserve(void)
 	msm_reserve();
 }
 
+#ifdef CONFIG_LCD_KCAL
+extern int g_kcal_r;
+extern int g_kcal_g;
+extern int g_kcal_b;
+
+int kcal_set_values(int kcal_r, int kcal_g, int kcal_b)
+{
+	g_kcal_r = kcal_r;
+	g_kcal_g = kcal_g;
+	g_kcal_b = kcal_b;
+	return 0;
+}
+
+static int kcal_get_values(int *kcal_r, int *kcal_g, int *kcal_b)
+{
+	*kcal_r = g_kcal_r;
+	*kcal_g = g_kcal_g;
+	*kcal_b = g_kcal_b;
+	return 0;
+}
+
+static int kcal_refresh_values(void)
+{
+	return update_preset_lcdc_lut();
+}
+
+static struct kcal_platform_data kcal_pdata = {
+	.set_values = kcal_set_values,
+	.get_values = kcal_get_values,
+	.refresh_display = kcal_refresh_values
+};
+
+static struct platform_device kcal_platrom_device = {
+	.name = "kcal_ctrl",
+	.dev = {
+		.platform_data = &kcal_pdata,
+	}
+};
+
+void __init add_lcd_kcal_devices(void)
+{
+	pr_info (" LCD_KCAL_DEBUG : %s \n", __func__);
+	platform_device_register(&kcal_platrom_device);
+};
+#endif
+
 /*
  * Used to satisfy dependencies for devices that need to be
  * run early or in a particular order. Most likely your device doesn't fall
@@ -139,6 +192,9 @@ void __init msm8226_add_drivers(void)
 	cpr_regulator_init();
 	tsens_tm_init_driver();
 	msm_thermal_device_init();
+#ifdef CONFIG_LCD_KCAL
+	add_lcd_kcal_devices();
+#endif
 }
 
 void __init msm8226_init(void)
