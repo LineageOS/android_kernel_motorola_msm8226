81870a6af4f52d69b28c482635025d731c253f0e
Deepak Kaushal <dkaushal@codeaurora.org>
2015-04-07 18:11:54 -0700

    msm: camera: Validate size param before allocating memory
    When ever i2c write is initiated check size param for NULL
    and in case of sequence write check for maximun allowed
    size per i2c sequence write.
    
    RM-290
    
    Change-Id: I111282537663d6b263a3686927c85b8f71560dae
    Signed-off-by: Deepak Kaushal <dkaushal@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/sensor/hi256.c
+++ drivers/media/platform/msm/camera_v2/sensor/hi256.c
@@ -1912,7 +1912,7 @@ int32_t hi256_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		}
 
 		if ((!conf_array.size) ||
-			(conf_array.size > I2C_SEQ_REG_DATA_MAX)) {
+			(conf_array.size > I2C_REG_DATA_MAX)) {
 			pr_err("%s:%d failed\n", __func__, __LINE__);
 			rc = -EFAULT;
 			break;
@@ -1953,7 +1953,7 @@ int32_t hi256_sensor_config(struct msm_sensor_ctrl_t *s_ctrl,
 		}
 
 		if ((!conf_array.size) ||
-			(conf_array.size > I2C_SEQ_REG_DATA_MAX)) {
+			(conf_array.size > I2C_REG_DATA_MAX)) {
 			pr_err("%s:%d failed\n", __func__, __LINE__);
 			rc = -EFAULT;
 			break;
