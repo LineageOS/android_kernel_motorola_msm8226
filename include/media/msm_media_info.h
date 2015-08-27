11233098030206761eb6817f201ebe2cebba22cd
Christopher Palmer <crpalmer@gmail.com>
2015-08-27 11:00:17 -0700

    msm: vidc: Always return 0 for EXTRADATA_SIZE
    * Userspace uses this value too, so both kernel and userspace need to be
      in agreement here.
    
    Change-Id: Ic216adf95bf4207c93089d611c5122edaeb25a04

--- include/media/msm_media_info.h
+++ include/media/msm_media_info.h
@@ -88,7 +88,7 @@ static inline unsigned int VENUS_EXTRADATA_SIZE(int width, int height)
 	 * In the future, calculate the size based on the w/h but just
 	 * hardcode it for now since 8K satisfies all current usecases.
 	 */
-#ifdef CONFIG_MMI_DEVICE_DTBS
+#if 1
 	return 0;
 #else
 	return 8 * 1024;
