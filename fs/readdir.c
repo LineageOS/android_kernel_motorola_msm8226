c6c3cb0c1d591fdbc4c5dce790166713b0d389b3
Tom Marshall <tdm.code@gmail.com>
2017-04-28 22:46:37 +0000

    kernel: Only expose su when daemon is running
    It has been claimed that the PG implementation of 'su' has security
    vulnerabilities even when disabled.  Unfortunately, the people that
    find these vulnerabilities often like to keep them private so they
    can profit from exploits while leaving users exposed to malicious
    hackers.
    
    In order to reduce the attack surface for vulnerabilites, it is
    therefore necessary to make 'su' completely inaccessible when it
    is not in use (except by the root and system users).
    
    [GabrieleM: Move some conditions not to error out when the current
     entry is 'su' and it should stay hidden]
    
    Change-Id: Ia7d50ba46c3d932c2b0ca5fc8e9ec69ec9045f85

--- fs/readdir.c
+++ fs/readdir.c
@@ -47,6 +47,14 @@ out:
 
 EXPORT_SYMBOL(vfs_readdir);
 
+static bool hide_name(const char *name, int namlen)
+{
+	if (namlen == 2 && !memcmp(name, "su", 2))
+		if (!su_visible())
+			return true;
+	return false;
+}
+
 /*
  * Traditional linux readdir() handling..
  *
@@ -68,6 +76,7 @@ struct old_linux_dirent {
 struct readdir_callback {
 	struct old_linux_dirent __user * dirent;
 	int result;
+	bool romnt;
 };
 
 static int fillonedir(void * __buf, const char * name, int namlen, loff_t offset,
@@ -77,6 +86,8 @@ static int fillonedir(void * __buf, const char * name, int namlen, loff_t offset
 	struct old_linux_dirent __user * dirent;
 	unsigned long d_ino;
 
+	if (buf->romnt && hide_name(name, namlen))
+		return 0;
 	if (buf->result)
 		return -EINVAL;
 	d_ino = ino;
@@ -116,6 +127,7 @@ SYSCALL_DEFINE3(old_readdir, unsigned int, fd,
 
 	buf.result = 0;
 	buf.dirent = dirent;
+	buf.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 
 	error = vfs_readdir(file, fillonedir, &buf);
 	if (buf.result)
@@ -144,6 +156,7 @@ struct getdents_callback {
 	struct linux_dirent __user * previous;
 	int count;
 	int error;
+	bool romnt;
 };
 
 static int filldir(void * __buf, const char * name, int namlen, loff_t offset,
@@ -156,6 +169,8 @@ static int filldir(void * __buf, const char * name, int namlen, loff_t offset,
 		sizeof(long));
 
 	buf->error = -EINVAL;	/* only used if we fail.. */
+	if (buf->romnt && hide_name(name, namlen))
+		return 0;
 	if (reclen > buf->count)
 		return -EINVAL;
 	d_ino = ino;
@@ -210,6 +225,7 @@ SYSCALL_DEFINE3(getdents, unsigned int, fd,
 	buf.previous = NULL;
 	buf.count = count;
 	buf.error = 0;
+	buf.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 
 	error = vfs_readdir(file, filldir, &buf);
 	if (error >= 0)
@@ -231,6 +247,7 @@ struct getdents_callback64 {
 	struct linux_dirent64 __user * previous;
 	int count;
 	int error;
+	bool romnt;
 };
 
 static int filldir64(void * __buf, const char * name, int namlen, loff_t offset,
@@ -242,6 +259,8 @@ static int filldir64(void * __buf, const char * name, int namlen, loff_t offset,
 		sizeof(u64));
 
 	buf->error = -EINVAL;	/* only used if we fail.. */
+	if (buf->romnt && hide_name(name, namlen))
+		return 0;
 	if (reclen > buf->count)
 		return -EINVAL;
 	dirent = buf->previous;
@@ -293,6 +312,7 @@ SYSCALL_DEFINE3(getdents64, unsigned int, fd,
 	buf.previous = NULL;
 	buf.count = count;
 	buf.error = 0;
+	buf.romnt = (file->f_path.dentry->d_sb->s_flags & MS_RDONLY);
 
 	error = vfs_readdir(file, filldir64, &buf);
 	if (error >= 0)
