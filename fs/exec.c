4e441ac21215e20bb168c9b7aec6abf754ff25a0
Gabriele M <moto.falcon.git@gmail.com>
2017-05-02 15:47:53 +0200

    Fix "Add PR_{GET,SET}_NO_NEW_PRIVS to prevent execve from granting privs"
    Commit 18630ad5638fd4dfd988af75f0b9d06fae91cac6
    ("Add PR_{GET,SET}_NO_NEW_PRIVS to prevent execve from granting privs")
    misses this line change, so add it now.
    
    Change-Id: I5036741683ef89189ab9b41b044bed19751a6d3c

--- fs/exec.c
+++ fs/exec.c
@@ -1270,7 +1270,8 @@ static void bprm_fill_uid(struct linux_binprm *bprm)
 	bprm->cred->euid = current_euid();
 	bprm->cred->egid = current_egid();
 
-	if (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID)
+	if ((bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID) ||
+	    current->no_new_privs)
 		return;
 
 	inode = bprm->file->f_path.dentry->d_inode;
